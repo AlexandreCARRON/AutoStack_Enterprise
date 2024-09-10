612d5dc8282047c487b1cb61216cd135a5cdacba
AlexandreCARRON <150245947+AlexandreCARRON@users.noreply.github.com>
2024-09-10 11:35:36 +0200

    Update script-conf-server-D12.sh
    ajout de l'installation de Prometheus Node exporter

--- script-conf-server-D12.sh
+++ script-conf-server-D12.sh
@@ -84,6 +84,43 @@ sudo systemctl restart fail2ban
 echo "######### Vérification de l'état du service Fail2ban après redémarrage"
 sudo systemctl status fail2ban
 
+# ********************************************************************* Installation de Prometheus Node Exporter *************************************
+# Télécharge et décompresse la version 1.8.2 de Node Exporter depuis le dépôt GitHub
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+tar xvfz node_exporter-1.8.2.linux-amd64.tar.gz
+
+# Déplace l'exécutable node_exporter dans le répertoire /usr/local/bin/ pour qu'il soit accessible globalement
+sudo mv node_exporter-1.8.2.linux-amd64/node_exporter /usr/local/bin/
+
+# Crée un utilisateur système appelé 'node_exporter' sans shell et sans répertoire home, pour des raisons de sécurité
+sudo useradd -rs /bin/false node_exporter
+
+# Copie le fichier de configuration service_node-config.txt dans le fichier de service systemd pour Node Exporter
+sudo cat ./host/Prometheus-Node-Exporter/service_node-config.txt > /etc/systemd/system/node_exporter.service
+
+# Recharge les fichiers de configuration systemd pour prendre en compte le nouveau service
+sudo systemctl daemon-reload
+
+# Démarre le service Node Exporter
+sudo systemctl start node_exporter
+
+# Active le service Node Exporter pour qu'il démarre automatiquement au démarrage de la machine
+sudo systemctl enable node_exporter
+
+# Vérifie le statut du service Node Exporter pour s'assurer qu'il fonctionne correctement
+sudo systemctl status node_exporter
+
+# (Optionnel) Ouvre le port 9100 sur le firewall pour autoriser les connexions TCP à Node Exporter (commenté ici)
+# sudo ufw allow 9100/tcp
+
+# Supprime l'archive téléchargée
+rm node_exporter-1.8.2.linux-amd64.tar.gz
+
+# Supprime le dossier extrait
+rm -rf node_exporter-1.8.2.linux-amd64
+
+echo "############################################# Les données Node Exporter sont maintenant disponibles via l'adresse <IP/DNS>:9100/metrics"
+
 # ********************************************************************* Changements MDP et DROITS *************************************
 # Changer le mot de passe pour l'utilisateur root
 echo "######### Changement du mots de passe pour l'utilisateurs root"
